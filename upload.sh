9d4cc997afef45a33b4cb7ed0ce64903d7d4167b
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2023-12-01 01:24:25 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 export UUID=${UUID:-'fd80f56e-93f3-4c85-b2a8-c77216c509a7'}
 export VPATH=${VPATH:-'vls'} 
 export CF_IP=${CF_IP:-'ip.sb'}
-export SUB_NAME=${SUB_NAME:-'argo'}
+export SUB_NAME=${SUB_NAME:-'vps'}
 export SUB_URL=${SUB_URL:-'https://xxxx'}
 
 
